283e66fb8a836ab326692ab429f6b2f87553a30d
tatsurosato <tatsu_241477@yahoo.co.jp>
2025-04-15 14:16:17 +0900

    task内容保持機能追加、未使用の背景画像の削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,70 @@
 <script>
 import { ref } from 'vue';
 import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
+import '@vuepic/vue-datepicker/dist/main.css';
 import { ja } from 'date-fns/locale';
+
+const LOCAL_STORAGE_KEY = 'todos'; // localStorageのキーを定数化
 
 export default {
   data() {
     return {
       newTodoText: '',
       todos: [],
+    };
+  },
+  created() {
+    if (process.client) { // クライアントサイドでのみ実行
+      const savedTodos = localStorage.getItem(LOCAL_STORAGE_KEY);
+      if (savedTodos) {
+        this.todos = JSON.parse(savedTodos);
+        this.todos.forEach(todo => {
+          if (todo.dueDate) {
+            todo.dueDate = new Date(todo.dueDate);
+          }
+        });
+      }
     }
+  },
+  watch: {
+    todos: {
+      handler(newTodos) {
+        if (process.client) { // クライアントサイドでのみ実行
+          localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTodos));
+        }
+      },
+      deep: true,
+    },
   },
   methods: {
     addTodo() {
-      if (!this.newTodoText) return alert('ToDoを入力してください')
+      if (!this.newTodoText) {
+        return alert('ToDoを入力してください');
+      }
       this.todos.push({
-      isDone: false,
-      text: this.newTodoText,
-      dueDate: null,
-      })
-        this.newTodoText = ''
-      },
+        isDone: false,
+        text: this.newTodoText,
+        dueDate: null,
+      });
+      this.newTodoText = '';
+      if (process.client) { // クライアントサイドでのみ実行
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.todos));
+      }
+    },
     delateTodo() {
-      this.todos = this.todos.filter(todo => !todo.isDone)
-    }
+      this.todos = this.todos.filter(todo => !todo.isDone);
+      if (process.client) { // クライアントサイドでのみ実行
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.todos));
+      }
+    },
   },
   components: { VueDatePicker },
   setup() {
     return {
-      ja
-    }
-  }
-}
+      ja,
+    };
+  },
+};
 </script>
 
 <template>
